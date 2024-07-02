dd0138cb087985ec12655aa3869b6b6bfedf7a60
cookieukw <65344982+cookieukw@users.noreply.github.com>
2024-07-02 16:51:48 -0300

    #

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,13 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-    appId: "com.cookie.bpocket",
-    appName: "Biblioteca Pocket",
-    webDir: "dist",
-    server: {
-        androidScheme: "https"
-    }
+  appId: "com.cookie.bpocket",
+  appName: "Biblioteca Pocket",
+  webDir: "dist",
+  server: {
+    hostname: "http://localhost:5174",
+    androidScheme: "https",
+  },
 };
 
 export default config;
--- src/screens/BookInfo.tsx
+++ src/screens/BookInfo.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import "./css/BookInfo.css";
 import { v4 } from "uuid";
 import { useLocation } from "react-router";
+import { FileOpener } from "@capacitor-community/file-opener";
+import { Filesystem, Directory } from "@capacitor/filesystem";
 
 import {
   IonPage,
@@ -24,7 +26,7 @@ import { useParams } from "react-router";
 import { getIcon, toCamelCase } from "../classes/util";
 import Lottie from "lottie-react";
 import animation404 from "../lottie/404.json";
-
+import { Capacitor } from "@capacitor/core";
 const url: string = "https://bpocket.vercel.app/api/book/";
 
 interface IBook {
@@ -42,7 +44,6 @@ interface IBook {
   abstract: string;
 }
 
-
 interface IBookContent {
   title: string;
   author: string;
@@ -53,6 +54,56 @@ interface IBookContent {
   link: string;
   bookId: string;
 }
+const downloadAndOpenFile = async (url: string) => {
+  console.log(url);
+
+  try {
+    // Use axios to fetch the file
+    const response = await fetch(url, {
+     // responseType: "blob",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Linux; Android 11; SM-A107M Build/RP1A.200720.012) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.91 Mobile Safari/537.36",
+        Cookie: "JSESSIONID=61D57D0C629DBCD3C6C767F21AC8D301",
+        Accept: "/",
+        Connection: "keep-alive",
+        "Access-Control-Allow-Origin": "http://localhost:5174",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      },
+    });
+    if (response.status !== 200) {
+      throw new Error("Network response was not ok");
+    }
+    /*
+    const blob = response.data;
+
+    const filename = url.substring(url.lastIndexOf("/") + 1);
+
+  
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = async () => {
+      const base64data = reader.result as string;
+      const base64 = base64data.split(",")[1];
+      const { uri } = await Filesystem.writeFile({
+        path: filename,
+        data: base64,
+        directory: Directory.Data,
+      });
+
+      const platform = Capacitor.getPlatform();
+      if (platform === "android" || platform === "ios") {
+      } else {
+        const blobUrl = URL.createObjectURL(blob);
+        // window.open(blobUrl, "_blank");
+      }
+    };*/
+  } catch (error) {
+    console.error("Error downloading and opening the file:", error);
+  }
+};
 
 const BookInfo: React.FC = () => {
   const [bookInfo, setBookInfo] = useState<IBook | null>(null);
@@ -60,9 +111,9 @@ const BookInfo: React.FC = () => {
   const { bookId } = useParams<{ bookId: string }>();
   const location = useLocation();
   const { state } = location;
-  
+
   //@ts-ignore
-  const bookContent:IBookContent = location.state?.book;
+  const bookContent: IBookContent = location.state?.book;
 
   console.log(state);
   useEffect(() => {
@@ -143,7 +194,7 @@ const BookInfo: React.FC = () => {
                     border: "1px solid black",
                     color: "black",
                   }}
-                  icon={getIcon(bookContent.format ?? "pdf")}
+                  icon={getIcon(bookContent?.format ?? "pdf")}
                   color="primary"
                   size="large"
                 />
@@ -172,7 +223,7 @@ const BookInfo: React.FC = () => {
                 <IonLabel>
                   <p>
                     Formato:{" "}
-                    {bookContent.format.substring(1).toUpperCase() ??
+                    {bookContent?.format.substring(1).toUpperCase() ??
                       "indisponível"}
                   </p>
                 </IonLabel>
@@ -191,7 +242,9 @@ const BookInfo: React.FC = () => {
               </IonCardContent>
             </IonCard>
             <IonButton
-              onClick={downloadHandler}
+              onClick={async () => {
+                await downloadAndOpenFile(bookInfo.downloadUrl);
+              }}
               style={{
                 "--background": "#e74c3c",
                 margin: "20px 0",
